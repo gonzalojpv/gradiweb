3fa5ab993813d2987467bdbdc72b75b1c50bf559
Gonzalo <gonzalojpv@gmail.com>
2020-09-05 03:46:37 -0500

    GP, Auth

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BaseController as BaseController;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Foundation\Auth\VerifiesEmails;
+use App\Http\Resources\UserResource;
+use Illuminate\Database\QueryException;
+use Validator;
+use Hash;
+
+class RegisterController extends BaseController
+{
+    use VerifiesEmails;
+    use SendsPasswordResetEmails, ResetsPasswords {
+        SendsPasswordResetEmails::broker insteadof ResetsPasswords;
+        ResetsPasswords::credentials insteadof SendsPasswordResetEmails;
+    }
+
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name'   => 'required',
+            'last_name'    => 'required',
+            'phone_number' => 'required',
+            'email'        => 'required|email',
+            'password'     => 'required',
+            'c_password'   => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $input['provider'] = 'web';
+
+        try {
+            $user = User::create($input);
+
+            $user->sendApiEmailVerificationNotification();
+        }
+        catch (QueryException $e){
+            $error_code = $e->errorInfo[1];
+            if ( $error_code == 1062 ) {
+                return $this->sendError('Correo electrónico ya registrado', ['error'=>'Duplicate']);
+            }
+        }
+
+        return $this->sendResponse(new UserResource($user), 'User register successfully.');
+    }
+
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+
+            if ( NULL !== $user->email_verified_at) {
+                return $this->sendResponse(new UserResource($user), 'User login successfully.');
+            }
+            else {
+                return $this->sendError('Please Verify Email.', ['error'=>'Please Verify Email']);
+            }
+        }
+        else{
+            return $this->sendError('Correo o contraseña incorrecta', ['error'=>'Unauthorised']);
+        }
+    }
+
+    public function callResetPassword(Request $request) {
+        return $this->reset($request);
+    }
+
+    /**
+     * Send password reset link.
+    */
+    public function sendPasswordResetLink(Request $request) {
+        return $this->sendResetLinkEmail($request);
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return $this->sendResponse($response, 'Password reset email sent.');
+    }
+
+    /**
+     * Get the response for a failed password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return $this->sendError('Email could not be sent to this email address.', ['error'=>'Email could not be sent to this email address.']);
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = Hash::make($password);
+        $user->save();
+        event(new PasswordReset($user));
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        return $this->sendResponse($response, 'Password reset successfully.');
+    }
+
+    /**
+     * Get the response for a failed password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return $this->sendError('Invalid Token.', ['error'=>'Invalid Token.']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'Api\RegisterController@login');
+Route::post('signup', 'Api\RegisterController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
